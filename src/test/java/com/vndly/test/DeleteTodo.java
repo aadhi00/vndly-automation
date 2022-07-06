c99c836406347d092ba84b51f32bceaa5d22f594
Caryen Cheong <caryen.cheong@sita.aero>
2022-07-06 11:41:40 +0800

    added forked code, and Chrome driver.

--- src/test/java/com/vndly/test/DeleteTodo.java
+++ src/test/java/com/vndly/test/DeleteTodo.java
@@ -1,12 +1,32 @@
 package com.vndly.test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class DeleteTodo {
 	
 	@Test
 	public void deleteTodo() {
-		//your code here
+		//find browser driver
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\src\\test\\resources\\driver\\chromedriver.exe");
+		
+		//initialize browser driver as 'driver'
+		WebDriver driver = new ChromeDriver();
+		
+		//load up the website to test
+		driver.get("https://qa-challenge.beta.vndly.com/");
+		
+		//select delete button element
+		WebElement deleteBtn = driver.findElement(By.xpath("//button[@data-testid='todo-list-item-delete']"));
+		
+		//automate clicking action to delete
+		deleteBtn.click();
+		
+		//output when the action is performed 
+		System.out.println("You have deleted a todo. Yay!");
 	}
 
 }
